63a3920a913259cc6b4610d877dad4b25295b471
David Mzareulyan <david@hiero.ru>
2016-01-13 20:21:54 +0300

    hash разбиваем по / и &

--- shorten.js
+++ shorten.js
@@ -20,6 +20,32 @@ module.exports = function (url, limit) {
     }
 
     if (hash) {
+        var p1 = hash.indexOf("/"),
+            p2 = hash.indexOf("&"),
+            s = "";
+        if (p1 === -1 && p2 === -1) {
+            // nope
+        } else if (p1 === -1) {
+            s = "&";
+        } else if (p2 === -1) {
+            s = "/";
+        } else if (p1 < p2) {
+            s = "/";
+        } else {
+            s = "&";
+        }
+
+        if (s !== "") {
+            parts = hash.split(s);
+            while (parts.length > 1) {
+                parts.pop();
+                href = buildHref(host, path, query, parts.join(s)) + s + "\u2026";
+                if (href.length <= limit) {
+                    return proto + href;
+                }
+            }
+        }
+
         href = buildHref(host, path, query) + "#\u2026";
         if (href.length <= limit) {
             return proto + href;
--- test/shorten-data.js
+++ test/shorten-data.js
@@ -8,5 +8,7 @@ module.exports = [
     "https://GooGle.com/?a=b&c=d&e=f&a=b&c=d&e=f",
     "https://github.com/octopress/ink#usage",
     "https://github.com/octopress/ink#usageusageusage",
-    "http://ezhikezhik.ru/reviews/amir-tagiev-esli-rebenku-ne-khvataet-obshcheniya-s-toboi-tvoya-vechnaya-rabota-i-zanyatost-vo-imya-ego-budushchego-nikomu-ne-nuzhnoe"
+    "http://ezhikezhik.ru/reviews/amir-tagiev-esli-rebenku-ne-khvataet-obshcheniya-s-toboi-tvoya-vechnaya-rabota-i-zanyatost-vo-imya-ego-budushchego-nikomu-ne-nuzhnoe",
+    "http://retromap.ru/m.php#l=0120090&r=081823&z=13&y=55.784687&x=37.567395",
+    "http://retromap.ru/m.php#sds/ddgfd/gdfdh/"
 ];
